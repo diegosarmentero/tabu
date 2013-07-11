0b50bab427fb40aae8b18277a844e8ed0d626692
Diego Sarmentero <diego.sarmentero@ninja-ide.org>
2013-07-11 00:18:16 -0300

    adding admin

--- web/tabu/tabu/urls.py
+++ web/tabu/tabu/urls.py
@@ -7,14 +7,16 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-     url(r'^$', views.home, name='home'),
-     url(r'^game/$', views.game_example, name='game_example'),
+    # Login
+    (r'^accounts/', include('allauth.urls')),
+    # Sections:
+    url(r'^$', views.home, name='home'),
+    url(r'^game/$', views.game_example, name='game_example'),
     # url(r'^tabu/', include('tabu.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
